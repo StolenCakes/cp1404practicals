72f88ab56ec1425936aa89e6ed34ef142f72b641
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-02-19 18:49:30 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,24 @@
-user_password = str(input("Enter password: "))
+"""
+CP1404/CP5632 - Practical
+Write a program that asks the user for a password, with error-checking to repeat if the password doesn't meet a minimum length set by a variable.
+The program should then print asterisks as long as the word.
+"""
+MINIMUM_PASSWORD_COUNT = 6
+MAXIMUM_PASSWORD_COUNT = 12
 
-letter_count = len(user_password)
+def main():
+    """Function docstring"""
+    letter_count = get_password()
+    print(letter_count * "*")
 
-print(letter_count * "*")
+def get_password():
+    """Check for a valid password"""
+    user_password = input("Enter password: ")
+    letter_count = len(user_password) # Counts letters in password entered
+    while letter_count < MINIMUM_PASSWORD_COUNT or letter_count > MAXIMUM_PASSWORD_COUNT:
+        print("Invalid score")
+        user_password = input("Enter password: ")
+        letter_count = len(user_password)
+    return letter_count
+
+main()
