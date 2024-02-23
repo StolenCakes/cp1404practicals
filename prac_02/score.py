e4756e37fca4e99e2944aadc4508dca147ee93f6
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-02-24 02:51:42 +0800

    Refactored broken_scores.py file

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,21 +1,37 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
-The intention is that the score must be between 0 and 100 inclusive;
-90 or more is excellent; 50 or more is a pass; below 50 is bad.
+Copy broken_score.py from prac 1 and rename it to score.py, then commit.
+
+Your main function should ask the user for their score and print the result.
+Write a new function that takes in the user's score as a parameter and returns the result to be printed.
+Follow SRP: The function should not print it.
+
+Now add a new part to the bottom of your main function that generates a random score and prints the result.
+You do NOT need to write a different function to determine the result for the random score.
+If you've written your new function properly, you can use it.
+If you've breached SRP, then you'll see that you can't.
 """
 MINIMUM_SCORE = 0
 MAXIMUM_SCORE = 100
 EXCELLENT_THRESHOLD = 90
 PASS_THRESHOLD = 50
 
-score = float(input("Enter score: "))
-while MINIMUM_SCORE <= score <= MAXIMUM_SCORE:
-    if score >= EXCELLENT_THRESHOLD:
-        print("Excellent")
-    elif score >= PASS_THRESHOLD:
-        print("Passable")
+def main():
+    user_score = float(input("Enter your score: "))
+    user_result = get_result(user_score)
+    print(f"Your result: {user_result}")
+
+def get_result(user_score):
+    """Return the result based on the given score."""
+    if MINIMUM_SCORE <= user_score <= MAXIMUM_SCORE:
+        if user_score >= EXCELLENT_THRESHOLD:
+            return "Excellent"
+        elif user_score >= PASS_THRESHOLD:
+            return "Passable"
+        else:
+            return "Bad"
     else:
-        print("Bad")
-    score = float(input("Enter score: "))
-print("Invalid score")
+        return "Invalid score"
+
+if __name__ == '__main__':
+    main()
